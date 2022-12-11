b57c0644d3632eb6062fdc5e8cff34ad7eb95ff5
hogang <hogang@johyogyeong-ui-MacBookAir.local>
2022-12-11 13:20:21 +0900

    create project

---
+++ SudokuBattle/SudokuBattle/Scenes/Auth/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  SudokuBattle
+//
+//  Created by hogang on 2022/12/11.
+//
+
+import UIKit
+import SnapKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
+
+private extension LoginViewController {
+    func setSubViews() {
+        
+    }
+}
